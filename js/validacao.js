ebf7cabf467adec33818b00fad3b12b201113df1
victor <jvictorpc@gmail.com>
2022-08-30 18:43:18 -0300

    validção da estrutura de cpf

--- js/validacao.js
+++ js/validacao.js
@@ -14,12 +14,14 @@ export function valida (input) {
         input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
     }
 }
+
 const tiposDeErro = [
     'valueMissing',
     'typeMismatch',
     'patternMismatch',
     'customError'
 ]
+
 const mensagensDeErro = {
     nome: {
         valueMissing: 'O campo nome não pode estar vazio'
@@ -35,6 +37,10 @@ const mensagensDeErro = {
     dataNascimento: {
         valueMissing: 'O campo data de nascimento não pode estar vazio',
         customError: 'Você deve ser maior que 18 anos para se cadastrar'
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio', 
+        customError: 'O CPF digitado não é válido'
     }
 
 
@@ -42,7 +48,8 @@ const mensagensDeErro = {
 }
 
 const validadores = {
-    dataNascimento: input => validaDataNascimento(input)
+    dataNascimento: input => validaDataNascimento(input),
+    cpf: input => validaCPF(input)
 }
 
 function mostraMensagemDeErro (tipoDeInput, input){
@@ -74,4 +81,73 @@ function maiorQue18 (data){
 
     return dataMaisque18 <= dataAtual;
 
+}
+
+function validaCPF (input){
+    const cpfFormatado = input.value.replace(/\D/g, '');
+    let mensagem = '';
+
+    if(!checaCpfRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)){
+        mensagem = 'O CPF digitado não é válido';
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function checaCpfRepetido (cpf){
+
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555', 
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ];
+
+    let cpfValido = true;
+
+    valoresRepetidos.forEach(valor => {
+        if(valor == cpf){
+            cpfValido = false;
+        }
+    })
+
+    return cpfValido;
+
+}
+
+function checaEstruturaCPF (cpf) {
+    const multiplicador = 10;
+
+    return checaDigitoVerificador(cpf, multiplicador);
+}
+
+function checaDigitoVerificador ( cpf, multiplicador ) {
+
+    if(multiplicador >= 12){
+        return true;
+    }
+    let multiplicadorInicial = multiplicador;
+    let soma = 0;
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('');
+    const digitoVerificador = cpf.charAt(multiplicador - 1);
+    for(let contador = 0; multiplicadorInicial >1; multiplicadorInicial-- ){
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial;
+        contador++;
+    }
+
+    if(digitoVerificador == confirmaDigito(soma)){
+        return checaDigitoVerificador(cpf, multiplicador + 1);
+    }
+
+    return false;
+}
+
+function confirmaDigito (){
+    return 11 - (soma%11);
 }
